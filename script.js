a06fc8225fae8407d7f1c2ae0738f3015ab6a592
Fernando Braga <88816612+fernando-braga@users.noreply.github.com>
2021-10-01 12:41:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/9/97/BackFuturePoster.jpg/225px-BackFuturePoster.jpg",
+  "https://br.web.img3.acsta.net/medias/nmedia/18/96/93/18/20497001.jpg",
+  "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG"
+];
+
+document.write("<img src=" + listaFilmes[0] + ">");
+document.write("<img src=" + listaFilmes[1] + ">");
+document.write("<img src=" + listaFilmes[2] + ">");
